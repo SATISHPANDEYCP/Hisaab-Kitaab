665d7f9d2dbc6b5f695b1c9f5264133b73c0c7a0
satishpandeycp <satishpandeycp@gmail.com>
2026-04-03 01:47:46 +0530

    performence

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui': ['react-icons', 'react-hot-toast'],
+          'charts': ['recharts'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
 })
